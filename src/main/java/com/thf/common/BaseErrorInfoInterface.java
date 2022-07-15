aab5ac3329c28c835e732a2ba4de42a1d2e59e5c
xzp <civilizationfor@outlook.com>
2022-07-15 15:07:03 +0800

    异常处理

---
+++ src/main/java/com/thf/common/BaseErrorInfoInterface.java
@@ -0,0 +1,21 @@
+package com.thf.common;
+/**
+ * @description: 服务接口类
+ * @author: DT
+ * @date: 2021/4/19 21:39
+ */
+public interface BaseErrorInfoInterface {
+
+    /**
+     *  错误码
+     * @return
+     */
+    String getResultCode();
+
+    /**
+     * 错误描述
+     * @return
+     */
+    String getResultMsg();
+}
+
