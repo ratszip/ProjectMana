a48b494e9612bf38803b97b4be46832c780f3fd1
xzp <civilizationfor@outlook.com>
2022-07-22 11:12:20 +0800

    debug JWT expire

--- src/main/java/com/thf/common/GloableVar.java
+++ src/main/java/com/thf/common/GloableVar.java
@@ -10,7 +10,7 @@ public class GloableVar {
     //手机正则
     public static String phoneReg="[\\+]?[0-9]{6,16}";
     //过期时长-毫秒
-    public static long expireTime=30*24*60*60*1000;
+    public static long expireTime=30*24*60*60*1000L;
     //验证码失效时间
     public static int codeExTime=5*60;
 }
