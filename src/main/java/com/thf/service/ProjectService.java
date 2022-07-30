c471ed94ca54833dc34b9213b75cdbc94e60f308
xzp <civilizationfor@outlook.com>
2022-07-30 17:32:36 +0800

    项目删除

--- src/main/java/com/thf/service/ProjectService.java
+++ src/main/java/com/thf/service/ProjectService.java
@@ -8,7 +8,7 @@ public interface ProjectService {
     ResultVO updateProject(String token,Project project);
     ResultVO searchById(long id,String token);
     ResultVO searchMyKey(String token,String key);
-//    ResultVO deleteProject(String token,Project project);
+    ResultVO deleteProject(String token,long[] pidArr);
     ResultVO getAllProjectDetail(String token,int type);
     ResultVO getList(String token,int type);
     ResultVO getProjectDetail(String token,long pid);
