56bbc1cbd52f2a4b5ff7126a5410036cd3e3f42d
xzp <civilizationfor@outlook.com>
2022-09-07 16:46:39 +0800

    home页面优化

--- src/main/java/com/thf/config/InterceptorConfig.java
+++ src/main/java/com/thf/config/InterceptorConfig.java
@@ -4,7 +4,6 @@ import com.thf.common.interceptors.CheckTokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,7 +14,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     private String UPLOAD_FOLDER;
     @Autowired
     private CheckTokenInterceptor checkTokenInterceptor;
-
+//    private static final List<String> EXCLUDE_PATH = Arrays.asList("/", "css/**", "js/**", "img/**", "json/**", "fonts/**","/*.html");
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(checkTokenInterceptor)
---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,24 @@
+$(function () {
+
+    var ipaddr = 'http://127.0.0.1/';
+
+    //全局的ajax访问，处理ajax清求时sesion超时
+    $.ajaxSetup({
+        contentType: "application/x-www-form-urlencoded;charset=utf-8",
+        complete: function (XMLHttpRequest, textStatus) {
+            var sessionstatus = XMLHttpRequest.getResponseHeader("sessionstatus"); // 通过XMLHttpRequest取得响应头，sessionstatus，
+            if (sessionstatus == "timeout") {
+                // 如果超时就处理 ，指定要跳转的页面
+                window.location.replace("login.html");
+            }
+        }
+    });
+    $('.add').click(function () {
+        // console("clicked");
+        $('.modal').css({"display":"block"});
+    });
+    $('.close').click(function () {
+        // console("clicked");
+        $('.modal').css({"display":"none"});
+    });
+})
