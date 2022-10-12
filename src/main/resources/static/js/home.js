d547a177116e4abb0a5142b78ac07ed32346d2e0
xzp <civilizationfor@outlook.com>
2022-10-12 16:37:47 +0800

    create pj post

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -23,4 +23,57 @@ $(function () {
     $('.cancelpj').click(function () {
             $('.modal').css({"display":"none"});
         });
+
+
+
+    $('.createpj').click(function(){
+    var projectName = $('.pjname').val();
+    var des = $('.pjdes').val();
+    var start=$('#beginpj').val();
+    var end=$('#endpj').val();
+        var json = {
+                       "projectName":projectName,
+                       "describe":des,
+                       "startTime":start,
+                       "endTime":end,
+                       "relateUser":20018
+                   };
+
+
+
+            $.ajax({
+                       //提交数据的类型 POST GET
+                       type: "POST",
+                       //提交的网址
+                       url: ipaddr + "project/create",
+                       //提交的数据
+                       data: JSON.stringify(json),
+                       xhrFields: { withCredentials: true },	//前端适配：允许session跨域
+                       crossDomain: true,
+
+                       //参数格式为json
+                       contentType: "application/json; charset=utf-8",
+                       //返回数据的格式
+                       datatype: "json",//"xml", "html", "script", "json", "jsonp", "text".
+                       //成功返回之后调用的函数
+                       success: function (data) {
+                           if (data.code === 2000) {
+                               $('#sendcode').css("cursor", "not-allowed");
+                               settime($("#sendcode"), 60);
+                           } else {
+                               alert(data.msg);
+                               return;
+                           }
+                           // console.log(typeof(data));
+                       },
+                       //调用出错执行的函数
+                       error: function () {
+                           $('#sendcode').css("cursor", "");
+                           //请求出错处理
+                           alert("请求失败");
+                           return;
+                       }
+
+                   });
+    });
 })
